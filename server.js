a1f46534fd3a0fa95b0b8037f6b442ba125b5843
Mohit <mr6114671@gmail.com>
2022-09-06 08:58:55 +0530

    first step of chat app

---
+++ server.js
@@ -0,0 +1,51 @@
+
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const socketIO = require("socket.io");
+
+const app = express();
+const port = 4500 || process.env.port;
+const server = http.createServer(app);
+const users = [{}];
+
+
+app.use(cors());
+app.get("/", (req, res) => {
+    res.send("hello");
+})
+
+
+
+const io = socketIO(server);
+
+io.on("connection", (socket) => {
+    console.log("New Connection");
+
+    socket.on('joined', ({ user }) => {
+        users[socket.id] = user;
+        console.log(`${user} has joined the chat room`);
+        socket.broadcast.emit('userjoined', { user: "Chat hub", message: `${users[socket.id]} has joined` })
+        socket.emit('welcome', { user: "Chat hub", message: `Welcome to the chat room, ${users[socket.id]} ` })
+
+        socket.on('disconnect', () => {
+            socket.broadcast.emit('left',{user:"Chat hub",message:`${users[socket.id]} has left`});
+            console.log('user left');
+        })
+      
+        
+        
+        socket.on('message',({message,id})=>{
+             io.emit('sendmessage',{user:users[id ],message,id})
+        })
+        
+    })
+
+
+});
+
+
+server.listen(port, () => {
+    console.log(`server working on http://localhost:${port}`);
+})
+
